210a21497aa2fd17a68ddfde603c6fc4c81cab79
tobia88 <tobia88@gmail.com>
2018-09-26 11:59:37 +0800

    Fix GetOrLossScore _isScreenPos default value as false

--- Scripts/TGGameScene.cs
+++ Scripts/TGGameScene.cs
@@ -113,7 +113,7 @@ public class TGGameScene : TGBaseScene
         GameState = GameStates.Start;
     }
 
-    public virtual void GetOrLossScore(int _score, Vector3 _position, bool _isScreenPos = true)
+    public virtual void GetOrLossScore(int _score, Vector3 _position, bool _isScreenPos = false)
     {
         Score += _score;
 
