705f93538ff76992ecf2560a2f5e00700aa48f66
tobia88 <tobia88@gmail.com>
2018-12-07 16:41:55 +0800

    Automation Export

---
+++ Editor/TGBuildGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using System.Linq;
+
+public class TGBuildGame : MonoBehaviour
+{
+	public static void Build()
+	{
+		string format = "{0}\\{1}\\{1}.exe";
+		string buildPath = string.Format(format, Environment.GetCommandLineArgs().Last(), Application.productName);
+		Debug.Log("Build Path: " + buildPath);
+		BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, buildPath, BuildTarget.StandaloneWindows64, BuildOptions.None);
+	}
+}
