c09fd7dadf3389623265ac1a5bf23c0f8092e147
tobia88 <tobia88@gmail.com>
2019-04-22 15:25:25 +0800

    Auto hide question mark btn if no tutorial image

--- Scripts/TGGameScene.cs
+++ Scripts/TGGameScene.cs
@@ -104,16 +104,24 @@ public class TGGameScene : TGBaseScene
     public override void Init()
     {
         base.Init();
+
+        var tutorialSpr = Resources.Load<Sprite>(controller.inputSetting.DeviceName);
+
         uiRoot.gameObject.SetActive(true);
         uiRoot.exitBtn.onClick.AddListener(OnPressExitGame);
         uiRoot.recalibrationBtn.onClick.AddListener(Recalibration);
-        uiRoot.questionBtn.onClick.AddListener(() => GameState = GameStates.Tutorial);
+
+        if (tutorialSpr != null)
+            uiRoot.questionBtn.onClick.AddListener(() => GameState = GameStates.Tutorial);
+
+        else
+            uiRoot.questionBtn.gameObject.SetActive(false);
 
         uiRoot.exitGamePanel.confirmBtn.onClick.AddListener(ExitScene);
         uiRoot.exitGamePanel.cancelBtn.onClick.AddListener(uiRoot.exitGamePanel.Exit);
         uiRoot.exitGamePanel.onFinishClosePanel += () => GameState = GameStates.Playing;
 
-        uiRoot.tutorialPanel.SetImage(Resources.Load<Sprite>(controller.inputSetting.DeviceName));
+        uiRoot.tutorialPanel.SetImage(tutorialSpr);
         uiRoot.tutorialPanel.confirmBtn.onClick.AddListener(uiRoot.tutorialPanel.Exit);
         uiRoot.tutorialPanel.onFinishClosePanel += () => GameState = GameStates.Playing;
 
